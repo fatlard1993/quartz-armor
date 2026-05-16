b1a156642dad19b825ea9615569bf4f861453d93
justfatlard <fatlard1993@gmail.com>
2026-05-16 16:32:51 -0700

    WIP

--- src/main/java/justfatlard/quartz_armor/Main.java
+++ src/main/java/justfatlard/quartz_armor/Main.java
@@ -1,157 +1,105 @@
 package justfatlard.quartz_armor;
 
-import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
-import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import justfatlard.pandorical.api.PandoricalApi;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.component.type.AttributeModifierSlot;
-import net.minecraft.component.type.AttributeModifiersComponent;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.equipment.EquipmentType;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.item.ItemGroups;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.item.equipment.EquipmentAssets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "quartz-armor-justfatlard";
 	private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// Quartz armor stats - high protection, low durability, high enchantability
-	public static final int BASE_DURABILITY = 10; // Very low (iron is 15, diamond is 33)
-	public static final int ENCHANTABILITY = 30; // Very high (diamond is 10, gold is 22)
-
-	// Defense values (higher than diamond!)
-	public static final int HELMET_DEFENSE = 4;
-	public static final int CHESTPLATE_DEFENSE = 9;
-	public static final int LEGGINGS_DEFENSE = 7;
-	public static final int BOOTS_DEFENSE = 4;
-
-	// Armor toughness (higher than diamond's 2)
-	public static final double ARMOR_TOUGHNESS = 3.0;
-
 	// Repair items tag
-	public static final TagKey<Item> REPAIR_ITEMS = TagKey.of(
-		RegistryKeys.ITEM, Identifier.of(MOD_ID, "quartz_armor_repair_items")
+	public static final TagKey<Item> REPAIR_ITEMS = TagKey.create(
+		Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, "quartz_armor_repair_items")
 	);
 
-	private static RegistryKey<Item> keyOf(String name) {
-		return RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name));
+	// Quartz armor material: high protection, low durability, high enchantability
+	public static final ArmorMaterial QUARTZ_ARMOR_MATERIAL = new ArmorMaterial(
+		10,                                    // durability (very low)
+		Map.of(                                // defense per slot (higher than diamond)
+			ArmorType.HELMET, 4,
+			ArmorType.CHESTPLATE, 9,
+			ArmorType.LEGGINGS, 7,
+			ArmorType.BOOTS, 4
+		),
+		30,                                    // enchantability (very high)
+		SoundEvents.ARMOR_EQUIP_DIAMOND,       // equip sound
+		3.0f,                                  // toughness (higher than diamond's 2)
+		0.0f,                                  // knockback resistance
+		REPAIR_ITEMS,                          // repair ingredient
+		EquipmentAssets.DIAMOND                // asset (visual fallback)
+	);
+
+	private static ResourceKey<Item> keyOf(String name) {
+		return ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, name));
 	}
 
-	private static AttributeModifiersComponent createArmorAttributes(int defense, double toughness, AttributeModifierSlot slot) {
-		return AttributeModifiersComponent.builder()
-			.add(
-				EntityAttributes.ARMOR,
-				new EntityAttributeModifier(
-					Identifier.of(MOD_ID, "armor_protection"),
-					defense,
-					EntityAttributeModifier.Operation.ADD_VALUE
-				),
-				slot
-			)
-			.add(
-				EntityAttributes.ARMOR_TOUGHNESS,
-				new EntityAttributeModifier(
-					Identifier.of(MOD_ID, "armor_toughness"),
-					toughness,
-					EntityAttributeModifier.Operation.ADD_VALUE
-				),
-				slot
-			)
-			.build();
-	}
+	// Creative tab key for combat
+	private static final ResourceKey<CreativeModeTab> COMBAT_TAB = ResourceKey.create(
+		Registries.CREATIVE_MODE_TAB, Identifier.fromNamespaceAndPath("minecraft", "combat")
+	);
 
-	// Armor items - using leather armor as Polymer item to enable custom color rendering on player model
+	// Armor items
 	public static final Item QUARTZ_HELMET = new QuartzArmorItem(
-		EquipmentType.HELMET,
-		new Item.Settings()
-			.registryKey(keyOf("quartz_helmet"))
-			.maxCount(1)
-			.maxDamage(EquipmentType.HELMET.getMaxDamage(BASE_DURABILITY))
-			.attributeModifiers(createArmorAttributes(HELMET_DEFENSE, ARMOR_TOUGHNESS, AttributeModifierSlot.HEAD))
-			.enchantable(ENCHANTABILITY)
-			.repairable(REPAIR_ITEMS),
-		Items.LEATHER_HELMET
+		new Item.Properties()
+			.setId(keyOf("quartz_helmet"))
+			.humanoidArmor(QUARTZ_ARMOR_MATERIAL, ArmorType.HELMET)
 	);
 
 	public static final Item QUARTZ_CHESTPLATE = new QuartzArmorItem(
-		EquipmentType.CHESTPLATE,
-		new Item.Settings()
-			.registryKey(keyOf("quartz_chestplate"))
-			.maxCount(1)
-			.maxDamage(EquipmentType.CHESTPLATE.getMaxDamage(BASE_DURABILITY))
-			.attributeModifiers(createArmorAttributes(CHESTPLATE_DEFENSE, ARMOR_TOUGHNESS, AttributeModifierSlot.CHEST))
-			.enchantable(ENCHANTABILITY)
-			.repairable(REPAIR_ITEMS),
-		Items.LEATHER_CHESTPLATE
+		new Item.Properties()
+			.setId(keyOf("quartz_chestplate"))
+			.humanoidArmor(QUARTZ_ARMOR_MATERIAL, ArmorType.CHESTPLATE)
 	);
 
 	public static final Item QUARTZ_LEGGINGS = new QuartzArmorItem(
-		EquipmentType.LEGGINGS,
-		new Item.Settings()
-			.registryKey(keyOf("quartz_leggings"))
-			.maxCount(1)
-			.maxDamage(EquipmentType.LEGGINGS.getMaxDamage(BASE_DURABILITY))
-			.attributeModifiers(createArmorAttributes(LEGGINGS_DEFENSE, ARMOR_TOUGHNESS, AttributeModifierSlot.LEGS))
-			.enchantable(ENCHANTABILITY)
-			.repairable(REPAIR_ITEMS),
-		Items.LEATHER_LEGGINGS
+		new Item.Properties()
+			.setId(keyOf("quartz_leggings"))
+			.humanoidArmor(QUARTZ_ARMOR_MATERIAL, ArmorType.LEGGINGS)
 	);
 
 	public static final Item QUARTZ_BOOTS = new QuartzArmorItem(
-		EquipmentType.BOOTS,
-		new Item.Settings()
-			.registryKey(keyOf("quartz_boots"))
-			.maxCount(1)
-			.maxDamage(EquipmentType.BOOTS.getMaxDamage(BASE_DURABILITY))
-			.attributeModifiers(createArmorAttributes(BOOTS_DEFENSE, ARMOR_TOUGHNESS, AttributeModifierSlot.FEET))
-			.enchantable(ENCHANTABILITY)
-			.repairable(REPAIR_ITEMS),
-		Items.LEATHER_BOOTS
+		new Item.Properties()
+			.setId(keyOf("quartz_boots"))
+			.humanoidArmor(QUARTZ_ARMOR_MATERIAL, ArmorType.BOOTS)
 	);
 
 	@Override
 	public void onInitialize() {
-		PolymerResourcePackUtils.addModAssets(MOD_ID);
-		PolymerResourcePackUtils.markAsRequired();
+		// Register with Pandorical if available
+		if (PandoricalApi.isAvailable()) {
+			PandoricalApi.content().registerModAssets(MOD_ID);
+		}
 
-		Registry.register(Registries.ITEM, keyOf("quartz_helmet"), QUARTZ_HELMET);
-		Registry.register(Registries.ITEM, keyOf("quartz_chestplate"), QUARTZ_CHESTPLATE);
-		Registry.register(Registries.ITEM, keyOf("quartz_leggings"), QUARTZ_LEGGINGS);
-		Registry.register(Registries.ITEM, keyOf("quartz_boots"), QUARTZ_BOOTS);
-
-		ItemGroup quartzArmorGroup = PolymerItemGroupUtils.builder()
-			.displayName(Text.literal("Quartz Armor"))
-			.icon(() -> new ItemStack(QUARTZ_CHESTPLATE))
-			.entries((context, entries) -> {
-				entries.add(new ItemStack(QUARTZ_HELMET));
-				entries.add(new ItemStack(QUARTZ_CHESTPLATE));
-				entries.add(new ItemStack(QUARTZ_LEGGINGS));
-				entries.add(new ItemStack(QUARTZ_BOOTS));
-			})
-			.build();
-		PolymerItemGroupUtils.registerPolymerItemGroup(Identifier.of(MOD_ID, "quartz_armor"), quartzArmorGroup);
+		Registry.register(BuiltInRegistries.ITEM, keyOf("quartz_helmet"), QUARTZ_HELMET);
+		Registry.register(BuiltInRegistries.ITEM, keyOf("quartz_chestplate"), QUARTZ_CHESTPLATE);
+		Registry.register(BuiltInRegistries.ITEM, keyOf("quartz_leggings"), QUARTZ_LEGGINGS);
+		Registry.register(BuiltInRegistries.ITEM, keyOf("quartz_boots"), QUARTZ_BOOTS);
 
 		// Add armor to vanilla Combat creative tab
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
-			entries.add(QUARTZ_HELMET);
-			entries.add(QUARTZ_CHESTPLATE);
-			entries.add(QUARTZ_LEGGINGS);
-			entries.add(QUARTZ_BOOTS);
+		CreativeModeTabEvents.modifyOutputEvent(COMBAT_TAB).register(entries -> {
+			entries.accept(QUARTZ_HELMET);
+			entries.accept(QUARTZ_CHESTPLATE);
+			entries.accept(QUARTZ_LEGGINGS);
+			entries.accept(QUARTZ_BOOTS);
 		});
 
-		LOGGER.info("Loaded quartz-armor (server-side with Polymer)");
+		LOGGER.info("Loaded quartz-armor (server-side with Pandorical)");
 	}
 }
--- src/main/java/justfatlard/quartz_armor/QuartzArmorItem.java
+++ src/main/java/justfatlard/quartz_armor/QuartzArmorItem.java
@@ -1,57 +1,9 @@
 package justfatlard.quartz_armor;
 
-import eu.pb4.polymer.core.api.item.PolymerItem;
-import eu.pb4.polymer.core.api.item.PolymerItemUtils;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.EquippableComponent;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.equipment.EquipmentType;
-import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.item.equipment.EquipmentAssetKeys;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
-import xyz.nucleoid.packettweaker.PacketContext;
+import net.minecraft.world.item.Item;
 
-public class QuartzArmorItem extends Item implements PolymerItem {
-	private final Item polymerItem;
-	private final EquipmentSlot slot;
-
-	// Custom equipment asset key
-	private static final RegistryKey<?> QUARTZ_EQUIPMENT_KEY = RegistryKey.of(
-		EquipmentAssetKeys.REGISTRY_KEY,
-		Identifier.of("minecraft", "quartz")
-	);
-
-	public QuartzArmorItem(EquipmentType type, Item.Settings settings, Item polymerItem) {
-		super(settings.equippable(type.getEquipmentSlot()));
-		this.polymerItem = polymerItem;
-		this.slot = type.getEquipmentSlot();
-	}
-
-	@Override
-	public Item getPolymerItem(ItemStack itemStack, PacketContext context) {
-		return this.polymerItem;
-	}
-
-	@Override
-	public ItemStack getPolymerItemStack(ItemStack itemStack, TooltipType tooltipType, PacketContext context) {
-		ItemStack out = PolymerItemUtils.createItemStack(itemStack, tooltipType, context);
-
-		// Create custom equippable component with our quartz equipment asset
-		@SuppressWarnings("unchecked")
-		EquippableComponent customEquippable = EquippableComponent.builder(this.slot)
-			.equipSound(SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND)
-			.model((RegistryKey) QUARTZ_EQUIPMENT_KEY)
-			.build();
-
-		out.set(DataComponentTypes.EQUIPPABLE, customEquippable);
-
-		// Remove dyed color since we're using a custom texture now
-		out.remove(DataComponentTypes.DYED_COLOR);
-
-		return out;
+public class QuartzArmorItem extends Item {
+	public QuartzArmorItem(Item.Properties settings) {
+		super(settings);
 	}
 }
